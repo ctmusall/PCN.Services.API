b50213b613a99735a1400cb2ae410dd95af179e8
Clayton <cmusall@pcnclosings.com>
2018-01-25 14:27:38 -0500

    Added service portal retrieve key

--- Services.Portal/Controllers/EmailController.cs
+++ Services.Portal/Controllers/EmailController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Services.Portal.Controllers
 {
@@ -22,6 +26,37 @@ namespace Services.Portal.Controllers
         public IActionResult Applications()
         {
             return PartialView("_ApplicationsPartial");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetToken()
+        {
+            // TODO - Retrieve token from API
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://localhost:11100"); // TODO - Move to appsettings and load in config object
+                    var content = new FormUrlEncodedContent(new []
+                    {
+                        new KeyValuePair<string, string>("applicationName", "Service.Portal") 
+                    });
+                    var response = await client.PostAsync("/token", content); // TODO - Move to appsettings and load in config object
+                    response.EnsureSuccessStatusCode();
+
+                    var x = response.Content.ReadAsStringAsync().Result;
+
+                    return Json(x);
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting emails from Email API: {httpRequestException.Message}");
+                }
+            }
+
+            // TODO - Sent Get request to API
+
+            // TODO - Return email data
         }
     }
 }
