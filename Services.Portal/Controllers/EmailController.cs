c7d8033d3d9dee77d0a7f0fcfcd6d84f8dbbea30
Clayton <cmusall@pcnclosings.com>
2018-01-26 12:09:46 -0500

    Retrieve emails and display in grid

--- Services.Portal/Controllers/EmailController.cs
+++ Services.Portal/Controllers/EmailController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +29,31 @@ namespace Services.Portal.Controllers
             return PartialView("_ApplicationsPartial");
         }
 
+
+        [HttpGet]
+        public async Task<IActionResult> GetEmails(string token)
+        {
+            // TODO - Sent Get request to API
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://localhost:11100"); // TODO - Move to appsettings and load in config object
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    var response = await client.GetAsync("/api/email"); 
+                    response.EnsureSuccessStatusCode();
+
+                    var x = response.Content.ReadAsStringAsync().Result;
+
+                    return Json(x);
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting emails from Email API: {httpRequestException.Message}");
+                }
+            }
+        }
+
         [HttpPost]
         public async Task<IActionResult> GetToken()
         {
@@ -41,7 +67,7 @@ namespace Services.Portal.Controllers
                     {
                         new KeyValuePair<string, string>("applicationName", "Service.Portal") 
                     });
-                    var response = await client.PostAsync("/token", content); // TODO - Move to appsettings and load in config object
+                    var response = await client.PostAsync("/token", content);
                     response.EnsureSuccessStatusCode();
 
                     var x = response.Content.ReadAsStringAsync().Result;
@@ -54,9 +80,7 @@ namespace Services.Portal.Controllers
                 }
             }
 
-            // TODO - Sent Get request to API
 
-            // TODO - Return email data
         }
     }
 }
